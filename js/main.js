9e2672fe33e50e29a38ad98f885bff0466e33d18
Enzo Romano <enzosromano@gmail.com>
2022-07-05 14:48:32 -0400

    Added on click functionality to highlight elements

--- js/main.js
+++ js/main.js
@@ -3,15 +3,13 @@ const container = document.querySelector('#container');
 var i = 0;
 
 while(i < 9){
-    const content = document.createElement('div')
-    content.classList.add('content' + i);
+    const content = document.createElement('button')
+    content.classList.add('content');
     content.textContent = 'Box: ' + i;
     container.appendChild(content);
+    content.onclick = function makeBlack() {
+        content.style.backgroundColor = "black";
+        container.style.color = "pink";
+    }
     i++;
 }
-
-/*const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
-
-container.appendChild(content);*/
