d45078d03b816f1254e71ce0b61def693c303ec7
Enzo Romano <enzosromano@gmail.com>
2022-07-06 11:52:12 -0400

    Added drag to draw functionality User can click or drag over elements to change color Expanded grid size

--- js/main.js
+++ js/main.js
@@ -2,14 +2,27 @@ const container = document.querySelector('#container');
 
 var i = 0;
 
-while(i < 9){
-    const content = document.createElement('button')
+while(i < 25){
+    const content = document.createElement('div')
     content.classList.add('content');
-    content.textContent = 'Box: ' + i;
+    /*content.textContent = 'Box: ' + i;*/
     container.appendChild(content);
-    content.onclick = function makeBlack() {
-        content.style.backgroundColor = "black";
-        container.style.color = "pink";
-    }
+
+    content.addEventListener('mousemove', function(event) {
+        if(event.buttons == 1) {
+         event.preventDefault();
+       
+         content.style.backgroundColor = "pink";
+       
+        }
+       });
+
+    content.addEventListener('mousedown', function(event) {
+
+         content.style.backgroundColor = "pink";
+       
+       });
+
+       
     i++;
 }
