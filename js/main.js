0fa9d9a97ed16063573a7f3328304e3ef624d1a6
Enzo Romano <enzosromano@gmail.com>
2022-07-07 15:20:48 -0400

    Removed grid gap and increased grid tiles

--- js/main.js
+++ js/main.js
@@ -3,29 +3,34 @@ const stuff = document.getElementsByClassName("content");
 
 var i = 0;
 
-while(i < 25){
-    const content = document.createElement('div')
-    content.classList.add('content');
-    /*content.textContent = 'Box: ' + i;*/
-    container.appendChild(content);
+initializeGridLayout();
 
-    content.addEventListener('mousemove', function(event) {
-        if(event.buttons == 1) {
-         event.preventDefault();
-       
-         content.style.backgroundColor = "pink";
-       
-        }
-       });
+function initializeGridLayout() {
 
-    content.addEventListener('mousedown', function(event) {
+    while(i < 100){
+        const content = document.createElement('div')
+        content.classList.add('content');
+        container.appendChild(content);
+    
+        content.addEventListener('mousemove', function(event) {
+            if(event.buttons == 1) {
+             event.preventDefault();
+           
+             content.style.backgroundColor = "pink";
+           
+            }
+           });
+    
+        content.addEventListener('mousedown', function(event) {
+    
+             content.style.backgroundColor = "pink";
+           
+           });
+    
+           
+        i++;
+    }
 
-         content.style.backgroundColor = "pink";
-       
-       });
-
-       
-    i++;
 }
 
 document.getElementById('clearDrawing').addEventListener("click", function(){
