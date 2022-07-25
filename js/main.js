49185fb96c54793a09905fe7a2517664b7b6223e
Enzo Romano <enzosromano@gmail.com>
2022-07-25 11:14:53 -0400

    Layout and Design Changes Changed the website to mimic an etch a sketch toy Changed flexbox alignments to be vertical Altered variable colors

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('#canvas');
 const stuff = document.getElementsByClassName("content");
 const slider = document.getElementById("slider");
 
