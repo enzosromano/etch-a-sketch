c5e5bd9c7b19e7bf93dcc6a701df3e77bf8eee30
Enzo Romano <enzosromano@gmail.com>
2022-07-26 10:52:29 -0400

    Javascript functionality additions Added standard, black and color mode to canvas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const container = document.querySelector('#canvas');
 const stuff = document.getElementsByClassName("content");
 const slider = document.getElementById("slider");
+var colorSetting = "gray";
 
-initializeGridLayout(slider.value);
+initializeGridLayout(slider.value, colorSetting);
 
-function initializeGridLayout(gridSize) {
+function initializeGridLayout(gridSize, colorSetting) {
 
     document.documentElement.style.setProperty('--gridSize', gridSize);
 
@@ -19,19 +20,36 @@ function initializeGridLayout(gridSize) {
             if(event.buttons == 1) {
              event.preventDefault();
            
-             content.style.backgroundColor = "#adadad";
+             content.style.backgroundColor = colorFunctionality(colorSetting);
            
             }
            });
     
         content.addEventListener('mousedown', function(event) {
     
-             content.style.backgroundColor = "#adadad";
+             content.style.backgroundColor = colorFunctionality(colorSetting);
            
            });
     
            
         i++;
+    }
+
+}
+
+function colorFunctionality(colorMode){
+
+    console.log(colorMode);
+
+    if(colorMode == "gray"){
+        return "gray";
+    }
+    else if(colorMode == "black"){
+        return "black";
+    }
+    else{
+        var colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+        return colors[Math.floor(Math.random() * colors.length)];
     }
 
 }
@@ -47,6 +65,33 @@ document.getElementById('clearDrawing').addEventListener("click", function(){
     for (var i = 0; i < stuff.length; i++) {
         stuff[i].style.backgroundColor = "white";
     }
+
+})
+
+document.getElementById('standardMode').addEventListener("click", function(){
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    initializeGridLayout(slider.value, "gray");
+
+})
+
+document.getElementById('blackMode').addEventListener("click", function(){
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    initializeGridLayout(slider.value, "black");
+
+})
+
+document.getElementById('colorMode').addEventListener("click", function(){
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    initializeGridLayout(slider.value, "rainbow");
 
 })
 
